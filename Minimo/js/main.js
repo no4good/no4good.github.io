ed69d6f9da5bcebd6d51c85627569ad12d0e0c37
no4good <no4goodll@gmail.com>
2019-04-16 22:52:31 +0300

    T2

---
+++ Minimo/js/main.js
@@ -0,0 +1,27 @@
+$('.spinner').css('display', 'none');
+$(function(){
+   $('.load-more').on('click', function(){
+      const btn = $(this);
+      const loader = btn.find('span');
+      $.ajax({
+       url: 'data.html',
+       type: 'GET',
+       beforeSend: function(){
+            btn.attr('disabled', true);
+         loader.addClass('d-inline-block');
+       },
+       success: function(responce){
+                setTimeout(function(){
+                   loader.removeClass('d-inline-block');
+                   btn.attr('disabled', false);
+                   $('.after-posts').before(responce);
+                }, 1000);
+       },
+       error: function(){
+          alert("err");
+          loader.removeClass('d-inline-block');
+          btn.attr('disabled', false);
+       }
+   });
+ });
+});
